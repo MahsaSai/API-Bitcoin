1d1c60fea990ffddad83f00d9237fc8646ca6aba
SaiSai <45283668+MahsaSai@users.noreply.github.com>
2022-04-20 12:13:26 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,8 @@
 import requests
 
 # Using a api link of a smsserver (REST API),
-# I have used Kavenegar that is a smsserver in my origin country
+# I have used Kavenegar that is a smsserver 
 def inform_me(price):
-    # API_Key = '6D654273375375464C4A627575356D746F59436C6350546F4B7A667A72502F424B63794B46786A556159773D'
     API_key = 'copy your api key of this smsserver here'
     url = 'https://api.kavenegar.com/v1/{}/sms/send.json'.format(API_Key)
     payload = {'receptor':'<add phone number here>', 'message':'Hi there, Bitcoin buy price is ${},\nFrom Mahsa'.format(price)}
